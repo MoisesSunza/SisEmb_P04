9df912004bc060a014eeb08d79e6b4fecff8481c
Moyete <al072954@uacam.mx>
2025-11-06 22:59:05 -0600

    Update main_server.py

--- proyecto_sensor_servo/main_server.py
+++ proyecto_sensor_servo/main_server.py
@@ -1,5 +1,3 @@
-# main_server.py (Pi 1: Servidor API)
-
 import threading
 import logging
 import time
@@ -11,16 +9,14 @@ from src.hardware.servo import ServoMotor # Se importa para la limpieza de GPIO
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # --- CONFIGURACIÓN DEL SISTEMA ---
-# DEFINICIÓN INTERACTIVA DE LA RESISTENCIA
 SERVO_PIN_DUMMY = 18 
-POTENTIOMETER_OHMS = 5000 # <--- ¡EL USUARIO DEFINE ESTE VALOR (ej: 5KΩ)!
+POTENTIOMETER_OHMS = 100000 # Definir valor del potenciometro
 
 if __name__ == '__main__':
     try:
         logging.info("--- Pi SERVER INICIADA (PUBLICADOR DE DATOS) ---")
         logging.info(f"--- RESISTENCIA DEL POTENCIÓMETRO CONFIGURADA EN: {POTENTIOMETER_OHMS} Ohms ---")
         
-        # 1. Crear una instancia del ServoMotor para tener el objeto si es necesario.
         dummy_servo = ServoMotor(SERVO_PIN_DUMMY, min_angle=0, max_angle=0)
         
         # 2. Iniciar el Servidor API, pasándole el valor de la resistencia
@@ -32,4 +28,5 @@ if __name__ == '__main__':
     finally:
         # 3. Limpieza final de recursos
         GPIO.cleanup() 
+
         logging.info("Pi Server: Sistema apagado y GPIO limpiado.")
